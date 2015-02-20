ff91ccc6c41fff1ffca363eb6e6392b3b42b0a87
Daniel <monttda@gmail.com>
2015-02-19 20:36:08 -0430

    Attempt to link existing account to the loging with github

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,11 +11,20 @@ class User < ActiveRecord::Base
 
 
   def self.from_omniauth(auth)
-      where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
-        user.provider = auth.provider
-        user.uid = auth.uid
-        user.email = auth.info.email
-        user.password = Devise.friendly_token[0,20]
+      email_match = User.find_by(email: auth.info.email)
+      credential_match = User.find_by(provider: auth.provider, uid: auth.uid)
+      if credential_match
+        credential_match
+      elsif email_match
+        email_match.update_attributes(uid: auth.uid,provider: auth.provider )
+        email_match.reload
+      else
+        User.create(
+          user.provider = auth.provider,
+          user.uid = auth.uid,
+          user.email = auth.info.email,
+          user.password = Devise.friendly_token[0,20]
+        )
       end
   end
 end
