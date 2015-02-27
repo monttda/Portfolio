ca49133a449c1f20cee8079841757f1284616e09
Daniel <monttda@gmail.com>
2015-02-27 01:17:36 -0430

    addition of validation for url format

--- app/models/story.rb
+++ app/models/story.rb
@@ -4,6 +4,7 @@ class Story < ActiveRecord::Base
   validates :user, presence: true
   validates :title, presence: true
   validate :presence_of_url_or_text
+  validate :url_has_correct_format
 =begin
     Returns all the comments related to the Story
     @return [Hash<Symbol,ActiveRecord_Associations_CollectionProxy>] a Hash
@@ -28,7 +29,13 @@ class Story < ActiveRecord::Base
     # Validates if the story has only a url or also a text
     def presence_of_url_or_text
       if self.url.present? && self.text.present?
-        errors.add(:url, "#{I18n.t('story.errors.url_text')}")
+        errors.add(:url, I18n.t('story.errors.url_text'))
+      end
+    end
+
+    def url_has_correct_format
+      unless self.url.downcase.start_with?('https://', 'http://')
+        errors.add(:url, I18n.t('story.errors.wrong_url_format'))
       end
     end
 end
